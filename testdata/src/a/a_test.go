c962731350e46d6e4b25cd082fd5abd288dd8481
tomato3713 <tomato3713@outlook.com>
2024-11-11 21:41:56 +0900

    append test case

--- testdata/src/a/a_test.go
+++ testdata/src/a/a_test.go
@@ -9,12 +9,18 @@ func Test_a(t *testing.T) {
 		t.Error("no match") // want "should not use external testing object."
 	}
 
-	assert2 := func(t *testing.T, a, b int) {
+	assert2 := func(t *testing.T, a int) {
 		t.Error("no match")
+	}
+
+	assert3 := func(a int, tt *testing.T) {
+		tt.Error("no match")
+		t.Error("no match") // want "should not use external testing object."
 	}
 
 	t.Run("sub test", func(t *testing.T) {
 		assert1()
-		assert2(t, 1, 2)
+		assert2(t, 1)
+		assert3(1, t)
 	})
 }
